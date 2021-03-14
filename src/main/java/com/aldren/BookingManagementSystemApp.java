c63bdd9ecd4865516caf5ed7e4e12983c3790371
iamaldren <aldrenbobiles@gmail.com>
2021-03-14 23:40:35 +0800

    Added embedded redis

--- src/main/java/com/aldren/BookingManagementSystemApp.java
+++ src/main/java/com/aldren/BookingManagementSystemApp.java
@@ -2,12 +2,30 @@ package com.aldren;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 @SpringBootApplication
 public class BookingManagementSystemApp
 {
+    private RedisServer redisServer;
+
     public static void main( String[] args )
     {
         SpringApplication.run(BookingManagementSystemApp.class, args);
     }
+
+    @PostConstruct
+    public void postConstruct() {
+        redisServer = new RedisServer();
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        redisServer.stop();
+    }
+
 }
